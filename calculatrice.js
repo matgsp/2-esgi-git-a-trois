db637dd6fa380b7c6fd083cfa55957478d315d7b
Raiwli <144922644+Raiwli@users.noreply.github.com>
2025-10-16 12:35:24 +0200

    ajout btn + fonction reset

---
+++ calculatrice.js
@@ -0,0 +1,5 @@
+function reset() {
+  document.getElementById("nombre1").value = "";
+  document.getElementById("nombre2").value = "";
+  document.getElementById("resultat").innerText = "Résultat : ";
+}
